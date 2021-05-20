b3701fe3472aacd791c0ac46f99efb60de74cfbd
Louis <53270473+Louis3797@users.noreply.github.com>
2021-05-20 14:05:23 +0200

    first do database integrration to finish this

--- bisasam/src/components/templates/Dashboard/Dashboard.tsx
+++ bisasam/src/components/templates/Dashboard/Dashboard.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import Feed from "../../modules/Feed";
 import Navbar from "../../modules/Navbar";
+
+import styles from "../../../styles/Dashboard.module.css";
 
 export const Dashboard: React.FC = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Navbar />
+      <div className={styles.innerContainer}>
+        <div className={styles.feedContainer}>
+          <Feed>
+            <h1>Hello World</h1>
+          </Feed>
+        </div>
+        <div className={styles.groupContainer}></div>
+      </div>
     </div>
   );
 };
