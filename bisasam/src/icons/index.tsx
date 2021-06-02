d80f92c6a138c14a998cc738f40b612f1b1dc33a
Louis <53270473+Louis3797@users.noreply.github.com>
2021-06-02 22:22:55 +0200

    Added Icons

---
+++ bisasam/src/icons/index.tsx
@@ -0,0 +1,7 @@
+export { default as SolidChatBubble } from "./SolidChatBubble";
+export { default as SolidLogOut } from "./SolidLogOut";
+export { default as SolidPlus } from "./SolidPlus";
+export { default as SolidRocket } from "./SolidRocket";
+export { default as SolidSearch } from "./SolidSearch";
+export { default as SolidSettings } from "./SolidSettings";
+export { default as SolidTrash } from "./SolidTrash";
