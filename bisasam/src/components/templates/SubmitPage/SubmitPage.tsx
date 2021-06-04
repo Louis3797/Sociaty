db20afd19f819e922a1fb40aebbf05b7fe1641f9
Louis <53270473+Louis3797@users.noreply.github.com>
2021-06-04 20:56:39 +0200

    Added features to Submit Page

--- bisasam/src/components/templates/SubmitPage/SubmitPage.tsx
+++ bisasam/src/components/templates/SubmitPage/SubmitPage.tsx
@@ -1,10 +1,21 @@
 import { useSession } from "next-auth/client";
 import { useRouter } from "next/dist/client/router";
+import { useState } from "react";
 import styles from "../../../styles/SubmitPage.module.scss";
+
+//Add error toast if u cant submit bc of text size
 
 const SubmitPage: React.FC = () => {
   const router = useRouter();
   const [session] = useSession();
+  const [text, settext] = useState("");
+
+  function handleSubmit(text: String) {
+    if (text.length <= 255) {
+      router.back();
+    }
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.innerContainer}>
@@ -14,9 +25,13 @@ const SubmitPage: React.FC = () => {
         <div className={styles.body}>
           <div className={styles.bodyContainerTop}>
             <img src={session.user.image} className={styles.userImg} />
-            <textarea placeholder="Was gibt es zu erzählen?" />
+            <textarea
+              placeholder="Was gibt es zu erzählen?"
+              onChange={(event) => settext(event.target.value)}
+            />
           </div>
           <div className={styles.bodyContainerBottom}>
+            <p>{text.length}/255</p>
             <button
               className={styles.buttonCancel}
               onClick={() => router.back()}
@@ -25,7 +40,7 @@ const SubmitPage: React.FC = () => {
             </button>
             <button
               className={styles.buttonSubmit}
-              onClick={() => router.back()}
+              onClick={() => handleSubmit(text)}
             >
               Submit
             </button>
---
+++ bisasam/src/pages/submit.tsx
@@ -0,0 +1,7 @@
+import SubmitPage from "../components/templates/SubmitPage/SubmitPage";
+
+const Submit: React.FC = () => {
+  return <SubmitPage />;
+};
+
+export default Submit;
