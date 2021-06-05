920ed69687b8b5601984b3224eb60e09b1531c68
Louis <53270473+Louis3797@users.noreply.github.com>
2021-06-06 01:11:40 +0200

    Added types

--- bisasam/src/components/templates/Dashboard/Dashboard.tsx
+++ bisasam/src/components/templates/Dashboard/Dashboard.tsx
@@ -1,29 +1,27 @@
 import Feed from "../../modules/Feed";
 import { useSession } from "next-auth/client";
-import { useQuery } from "@apollo/client";
+import { ApolloError, useQuery } from "@apollo/client";
 import { useEffect } from "react";
 import { GET_USER_ID } from "../../../graphql/querys";
-
 interface ID {
   id: Number;
 }
 
 interface ID_DATA {
   findUserWithEmail: ID;
+  loading: boolean;
+  error: ApolloError;
 }
 
 export const Dashboard: React.FC = () => {
   const [session] = useSession();
 
   // Get the email of the current User
-  const userEmail = session?.user.email.toString();
+  const userEmail: string = session?.user.email.toString();
 
   // Query with the current User email
   const { loading, error, data } = useQuery<ID_DATA>(GET_USER_ID, {
     variables: { email: userEmail.toString() },
-
-    // After 5 min check again
-    pollInterval: 50000,
   });
 
   useEffect(() => {
