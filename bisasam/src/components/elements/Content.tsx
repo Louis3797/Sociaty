6d69d35be717c309885cb7130928bb2ac62baa11
Louis <53270473+Louis3797@users.noreply.github.com>
2021-06-06 02:33:16 +0200

    Responsive designs and  UserPage changes

--- bisasam/src/components/elements/Content.tsx
+++ bisasam/src/components/elements/Content.tsx
@@ -5,15 +5,24 @@ export interface ContentProps {
   name: string;
   text?: string;
   img?: string;
+  commentAmount: number;
+  likeAmount: number;
 }
 
-const Content: React.FC<ContentProps> = ({ userImg, name, text, img }) => {
+const Content: React.FC<ContentProps> = ({
+  userImg,
+  name,
+  text,
+  img,
+  commentAmount,
+  likeAmount,
+}) => {
   return (
     <div className="flex flex-col w-full h-auto bg-primary mb-4 rounded-3xl justify-evenly">
-      <div className="flex flex-row w-full h-1/5 items-center bg-transparent">
+      <div className="flex flex-row w-full h-1/5 items-center bg- pt-6 pb-4">
         <img
           src={userImg}
-          className="h-4/5 w-auto rounded-full object-cover ml-5 mr-4"
+          className="h-10 w-auto rounded-full object-cover ml-5 mr-4"
           alt="UserImg"
         />
         <p className="text-lg font-semibold tracking-wide">{name}</p>
@@ -23,8 +32,14 @@ const Content: React.FC<ContentProps> = ({ userImg, name, text, img }) => {
       </div>
       <div className="items-center justify-around bg-transparent p-1 flex flex-row w-full h-26">
         <ShareIcon className="text-base" />
-        <SolidChatBubble className="text-base" />
-        <SolidRocket className="text-base" />
+        <div className="flex flex-row items-center">
+          <SolidChatBubble className="text-base" />
+          <p className="text-sm font-light ml-3">{commentAmount}</p>
+        </div>
+        <div className="flex flex-row items-center">
+          <SolidRocket className="text-base" />
+          <p className="text-sm font-light ml-3">{likeAmount}</p>
+        </div>
       </div>
     </div>
   );
