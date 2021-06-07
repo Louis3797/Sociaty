2b03e78fee711834f80676ee298be05e2a03f055
Louis <53270473+Louis3797@users.noreply.github.com>
2021-06-08 01:05:49 +0200

    added new button types

--- bisasam/src/components/templates/SubmitPage/SubmitPage.tsx
+++ bisasam/src/components/templates/SubmitPage/SubmitPage.tsx
@@ -1,10 +1,11 @@
 import { useMutation } from "@apollo/client";
 import { useSession } from "next-auth/client";
 import { useRouter } from "next/dist/client/router";
-import { useState } from "react";
+import React, { useState } from "react";
 import { POST_CONTENT } from "../../../graphql/mutations";
-import CancelButton from "../../elements/button/CancelButton";
-import SubmitButton from "../../elements/button/SubmitButton";
+import Button from "../../elements/button/Button";
+import ButtonLink from "../../elements/button/ButtonLink";
+import ButtonOutlined from "../../elements/button/ButtonOutlined";
 
 const SubmitPage: React.FC = () => {
   const router = useRouter();
@@ -29,7 +30,7 @@ const SubmitPage: React.FC = () => {
 
   return (
     <div className="flex flex-col w-full h-full bg-transparent items-center">
-      <div className="block bg-primary-800 2xl:w-2/5 lg:w-3/5 md:w-2/5 h-64 rounded-20 mt-24">
+      <div className="block bg-primary-800 2xl:w-2/5 lg:w-3/5 md:w-2/5 h-64 rounded-8 mt-24">
         <div className="flex w-full h-1/6 items-start">
           <h1 className="text-2xl font-semibold font-comfortaa text-primary-100 ml-5 mt-3">
             Create a Post
@@ -51,22 +52,25 @@ const SubmitPage: React.FC = () => {
             <p className="text-base font-normal  text-secondary-600 mb-4">
               {text.length}/255
             </p>
-            <div className="ml-3 mb-3 mr-3 w-auto h-auto">
-              <CancelButton
+            <div className="ml-5 mb-4 mr-5 w-auto h-auto">
+              <ButtonLink
+                disabled={false}
                 text="Cancel"
                 click={() => {
-                  handleSubmit(text);
+                  router.back();
                   settext("");
                 }}
               />
             </div>
-            <div className="mb-3 mr-3 w-auto h-auto">
-              <SubmitButton
+            <div className="mb-3 mr-5 w-auto h-auto">
+              <Button
                 text="Submit"
                 disabled={text.length > 255 || text.length === 0}
                 click={() => {
                   handleSubmit(text);
                 }}
+                variant="primary"
+                size="big"
               />
             </div>
           </div>
