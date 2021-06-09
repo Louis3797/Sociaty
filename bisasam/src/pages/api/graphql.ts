df024aae99ed9688c9e576cdc1dd6b1f3359f6f0
Louis <53270473+Louis3797@users.noreply.github.com>
2021-06-09 23:32:31 +0200

    improvements and new querys

--- bisasam/src/pages/api/graphql.ts
+++ bisasam/src/pages/api/graphql.ts
@@ -3,15 +3,16 @@ import { ApolloServer, gql } from "apollo-server-micro";
 
 const typeDefs = gql`
   type Query {
-    allUsers: [User!]!
     findUser(id: Int!): User!
     findUserWithEmail(email: String!): User!
+    getContent(content_id: Int!, userId: Int!): Content!
   }
 
   type Mutation {
     postContent(content_text: String!, userId: Int!, image_id: Int): Content
     deleteContentLike(userId: Int!, content_id: Int!): UserLikedContent
     createContentLike(userId: Int!, content_id: Int!): UserLikedContent
+    postComment(content_id: Int!, comment_text: String!, userId: Int!): Comment
   }
   type User {
     id: Int!
@@ -19,17 +20,21 @@ const typeDefs = gql`
     image: String!
     email: String!
     bio: String
+    created_at: String!
     content: [Content!]
-    followedBy: [UserFollowsUser!]
-    following: [UserFollowsUser!]
-    liked_content: [UserLikedContent!]
-    liked_comments: [UserLikedComment!]
+    followedBy: FollowedBy
+    following: Following
+    liked_content: [UserLikedContent]
+    liked_comments: [UserLikedComment]
     blockedBy: [User]
     blocked: [User]
   }
-  type UserFollowsUser {
-    userA: User!
-    userB: User!
+  type Following {
+    user: [User]
+  }
+
+  type FollowedBy {
+    user: [User]
   }
 
   type Content {
@@ -37,6 +42,7 @@ const typeDefs = gql`
     content_text: String!
     userId: Int!
     image_id: Int
+    created_at: String!
     comments: [Comment]
     liked: [UserLikedContent]
     user: User!
@@ -62,16 +68,14 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    allUsers: () => {
-      return prisma.user.findMany();
-    },
-
     findUser: (_parent, _args, ctx) => {
       return prisma.user.findUnique({
         where: {
           id: _args.id,
         },
         include: {
+          followedBy: true,
+          following: true,
           liked_content: {
             where: {
               userId: _args.id,
@@ -79,11 +83,7 @@ const resolvers = {
           },
           content: {
             include: {
-              liked: {
-                where: {
-                  userId: _args.id,
-                },
-              },
+              liked: true,
               comments: true,
               user: true,
             },
@@ -102,7 +102,21 @@ const resolvers = {
         },
       });
     },
+    getContent: (_parent, _args, ctx) => {
+      return prisma.content.findMany({
+        where: {
+          content_id: _args.content_id,
+          userId: _args.userId,
+        },
+        include: {
+          liked: true,
+          comments: true,
+          user: true,
+        },
+      });
+    },
   },
+
   Mutation: {
     postContent: (_parent, _args, ctx) => {
       return prisma.content.create({
@@ -125,6 +139,15 @@ const resolvers = {
         where: {
           userId: _args.userId,
           content_id: _args.content_id,
+        },
+      });
+    },
+    postComment: (_parent, _args, ctx) => {
+      return prisma.user_comment.create({
+        data: {
+          content_id: _args.content_id,
+          userId: _args.userId,
+          comment_text: _args.comment_text,
         },
       });
     },
