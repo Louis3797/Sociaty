b3d8b218028fff308a7a14dd147035d83280c929
Louis <53270473+Louis3797@users.noreply.github.com>
2021-06-09 23:31:03 +0200

    Added real Follower stats

--- bisasam/src/components/templates/UserPage/UserPage.tsx
+++ bisasam/src/components/templates/UserPage/UserPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Content from "../../elements/content/Content";
+import Content from "../../modules/content/Content";
 import ProfileFeed from "../../modules/profile/ProfileFeed";
 import ProfileHeader from "../../modules/profile/ProfileHeader";
 import ProfileInfoBox from "../../modules/profile/ProfileInfoBox";
@@ -13,7 +13,6 @@ interface UserPageProps {
 }
 
 const UserPage: React.FC<UserPageProps> = ({ data }) => {
-  console.log(data);
   const userContent = data.findUser.content.map((content) => {
     return (
       <Content
@@ -25,12 +24,11 @@ const UserPage: React.FC<UserPageProps> = ({ data }) => {
         text={content.content_text}
         likeAmount={content.liked === null ? 0 : content.liked.length}
         commentAmount={content.comments === null ? 0 : content.comments.length}
-        //How can we proof faster
-        liked={content.liked.length === 1 ? true : false}
+        liked={content.liked.length === 1}
       />
     );
   });
-
+  console.log(data.findUser);
   return (
     <div className="flex flex-col w-full h-screen bg-primary-900 items-center">
       <div className="flex flex-col max-w-2xl items-center bg-transparent mt-15 h-auto">
@@ -40,7 +38,19 @@ const UserPage: React.FC<UserPageProps> = ({ data }) => {
           email={data.findUser.email}
           bio={data.findUser.bio ? data.findUser.bio : "Hey im new here"}
         />
-        <ProfileInfoBox follower={0} follows={0} posts={0} />
+        <ProfileInfoBox
+          follower={
+            data.findUser.followedBy.user === null
+              ? 0
+              : data.findUser.followedBy.user.length
+          }
+          follows={
+            data.findUser.following.user === null
+              ? 0
+              : data.findUser.following.user.length
+          }
+          posts={data.findUser.content.length}
+        />
         <div className="flex flex-col w-full h-full bg-transparent items-center justify-start mt-10">
           {userContent}
         </div>
