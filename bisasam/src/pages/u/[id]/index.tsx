367eee72eaf276fdaac604013faae2cf8fedee24
Louis <53270473+Louis3797@users.noreply.github.com>
2021-06-21 01:02:14 +0200

    added types

--- bisasam/src/pages/u/[id]/index.tsx
+++ bisasam/src/pages/u/[id]/index.tsx
@@ -1,10 +1,52 @@
 import UserPage from "../../../components/templates/UserPage/UserPage";
-interface USER {
-  findUser: any;
+
+export interface Hashtags {
+  text: string;
+}
+export interface ContentUser {
+  __typename: string;
+  id: string;
+  displayName: string;
+  name: string;
+  image: string;
+}
+
+export interface Content {
+  __typename: string;
+  id: string;
+  content_text: string;
+  userId: string;
+  image_id?: any;
+  created_at: Date;
+  numLikes: number;
+  numComments: number;
+  gif_url?: string;
+  tags?: Hashtags;
+  user: ContentUser;
+}
+
+export interface FindUser {
+  __typename: string;
+  id: string;
+  name: string;
+  displayName: string;
+  image: string;
+  bannerUrl?: any;
+  bio?: any;
+  created_at: Date;
+  numFollowing: number;
+  numFollowers: number;
+  numContributions: number;
+  online: boolean;
+  content: Content[];
+}
+
+export interface RootProps {
+  findUser: FindUser;
 }
 
 interface ProfilePageProps {
-  data?: USER;
+  data: RootProps;
 }
 
 const ProfilePage: React.FC<ProfilePageProps> = ({ data }) => {
