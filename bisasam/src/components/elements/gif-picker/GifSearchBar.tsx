94a8e5b358961843c1f0daf565d03273be62a5c2
Louis <53270473+Louis3797@users.noreply.github.com>
2021-06-20 22:28:29 +0200

    added giphy api and build gif picker

---
+++ bisasam/src/components/elements/gif-picker/GifSearchBar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ButtonIcon from "../button/ButtonIcon";
+import CloseRoundedIcon from "@material-ui/icons/CloseRounded";
+interface GifPickerSearchBarProps {
+  onChange: (e) => void;
+  close: () => void;
+}
+
+const GifPickerSearchBar: React.FC<GifPickerSearchBarProps> = ({
+  onChange,
+  close,
+}) => {
+  return (
+    <div className="flex flex-row w-full h-full items-center justify-between bg-primary-800 rounded-8">
+      <ButtonIcon
+        size="big"
+        bgcolor="bg-secondary-600"
+        click={close}
+        className="mx-3 p-3"
+      >
+        <CloseRoundedIcon fontSize="default" className="text-secondary-600" />
+      </ButtonIcon>
+      <input
+        type="text"
+        className="w-full h-4/5 rounded-8 p-2 mx-3 bg-primary-600 outline-none"
+        onChange={onChange}
+        placeholder="Search for Gifs..."
+      />
+    </div>
+  );
+};
+
+export default GifPickerSearchBar;
---
+++ bisasam/src/components/modules/picker/GifPicker.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+import Modal from "../../elements/modal/Modal";
+import GifPickerSearchBar from "../../elements/gif-picker/GifSearchBar";
+import GifResults from "../../elements/gif-picker/GifResults";
+
+interface GifPickerProps {
+  className?: string;
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+const GifPicker: React.FC<GifPickerProps> = ({
+  className,
+  isOpen,
+  onRequestClose,
+}) => {
+  const [search, setsearch] = useState("");
+
+  function handleCancel() {
+    onRequestClose();
+    setsearch("");
+  }
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={handleCancel}
+      className={`flex justify-center items-start focus:outline-none border-0 ${className}`}
+    >
+      <div className="flex flex-col w-42 h-48 bg-primary-800 rounded-8">
+        <div className="flex items-center w-full h-7">
+          <GifPickerSearchBar
+            onChange={(e) => {
+              setsearch(e.target.value);
+            }}
+            close={handleCancel}
+          />
+        </div>
+        <div className="flex items-center justify-end bg-transparent px-4 py-1">
+          <p className="font-bold text-xs text-primary-200 opacity-50">
+            Gifs by GIPHY
+          </p>
+        </div>
+        <div className="flex flex-col w-42 bg-transparent h-full rounded-b-8 overflow-y-scroll">
+          <GifResults gifText={search} close={handleCancel} />
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default GifPicker;
