e117451a43a2a738f0f406cd1db3e67151dbc799
Louis <53270473+Louis3797@users.noreply.github.com>
2021-06-23 23:12:15 +0200

    updated and created queries

--- bisasam/src/graphql/querys.ts
+++ bisasam/src/graphql/querys.ts
@@ -11,7 +11,7 @@ export const GET_USER_ID = gql`
 
 export const GET_USER = gql`
   query GET_USER($id: String!) {
-    findUser(id: $id) {
+    getUserData(id: $id) {
       id
       name
       displayName
@@ -23,6 +23,17 @@ export const GET_USER = gql`
       numFollowers
       numContributions
       online
+    }
+  }
+`;
+
+export const GET_USER_CONTENT = gql`
+  query GET_USER_CONTENT($userId: String!, $currentUserId: String!) {
+    getUserContent(userId: $userId, currentUserId: $currentUserId) {
+      id
+      name
+      displayName
+      image
       content {
         id
         content_text
@@ -32,44 +43,40 @@ export const GET_USER = gql`
         numLikes
         numComments
         gif_url
+        favourite
         tags {
           hashtag {
+            id
             text
           }
-        }
-        user {
-          id
-          displayName
-          name
-          image
         }
       }
     }
   }
 `;
 
-export const GET_CONTENT = gql`
-  query GET_CONTENT($userId: Int!, $content_id: Int!) {
-    getContent(userId: $userId, content_id: $content_id) {
-      content_id
-      content_text
-      userId
-      image_id
-      created_at
-      user {
-        name
-        image
-      }
-      liked {
-        userId
-      }
-      comments {
-        comment_id
-        content_id
-        comment_text
-        userId
-        created_at
-      }
-    }
-  }
-`;
+// export const GET_CONTENT = gql`
+//   query GET_CONTENT($userId: String!, $content_id: String!) {
+//     getSingleContent(userId: $userId, content_id: $content_id) {
+//       content_id
+//       content_text
+//       userId
+//       image_id
+//       created_at
+//       user {
+//         name
+//         image
+//       }
+//       liked {
+//         userId
+//       }
+//       comments {
+//         comment_id
+//         content_id
+//         comment_text
+//         userId
+//         created_at
+//       }
+//     }
+//   }
+// `;
