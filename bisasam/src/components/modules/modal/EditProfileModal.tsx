9cc40ad19b0ffb393b7e54d47eb685f671331629
Louis <53270473+Louis3797@users.noreply.github.com>
2021-07-04 22:40:46 +0200

    finished EPModal

--- bisasam/src/components/modules/modal/EditProfileModal.tsx
+++ bisasam/src/components/modules/modal/EditProfileModal.tsx
@@ -1,23 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ButtonIcon from "../../elements/button/ButtonIcon";
 import Modal from "../../elements/modal/Modal";
 
 import CloseRoundedIcon from "@material-ui/icons/CloseRounded";
 import Button from "../../elements/button/Button";
+import { InputField } from "../../elements/input/InputField";
+import SingleUserAvatar from "../../elements/UserAvatar/SingleUserAvatar";
+import { useSession } from "next-auth/client";
+import { UPDATE_PROFILE } from "../../../graphql/mutations";
+import { OperationVariables, useMutation } from "@apollo/client";
 
 export interface EditProfileModalProps {
   className?: string;
   isOpen: boolean;
   onRequestClose: () => void;
+  bannerUri: string | null;
+  displayedName: string;
+  bio: string | null;
 }
 
 const EditProfileModal: React.SFC<EditProfileModalProps> = ({
   className,
   isOpen,
   onRequestClose,
+  bannerUri,
+  displayedName,
+  bio,
 }) => {
-  const [disabled, setDisabled] = useState(false);
-
   function handleCancel() {
     onRequestClose();
   }
@@ -27,11 +36,12 @@ const EditProfileModal: React.SFC<EditProfileModalProps> = ({
       onRequestClose={handleCancel}
       className={`flex justify-center items-start focus:outline-none border-0 ${className}`}
     >
-      <div className="flex flex-col w-42 sm:w-full rounded-8 h-15  bg-primary-800">
-        <EPModalHeader
-          close={handleCancel}
-          save={() => {}}
-          disabled={disabled}
+      <div className="flex flex-col w-42 sm:w-full rounded-8 h-auto bg-primary-800 overflow-y-auto">
+        <EPModalHeader close={handleCancel} />
+        <EPModalBody
+          bannerUri={bannerUri}
+          displayedName={displayedName}
+          bio={bio}
         />
       </div>
     </Modal>
@@ -42,23 +52,17 @@ export default EditProfileModal;
 
 interface EPModalHeaderProps {
   close: () => void;
-  save: () => void;
-  disabled: boolean;
 }
 
-const EPModalHeader: React.FC<EPModalHeaderProps> = ({
-  close,
-  save,
-  disabled,
-}) => {
+const EPModalHeader: React.FC<EPModalHeaderProps> = ({ close }) => {
   return (
     <div className="flex items-center w-full h-7">
-      <div className="flex flex-row w-full h-full items-center justify-between bg-primary-800 rounded-8">
+      <div className="flex flex-row w-full h-full py-2 items-center justify-between bg-primary-800 rounded-8">
         <div className="flex flex-row items-center justify-start ml-3">
           <ButtonIcon
             size="big"
             bgcolor="bg-secondary-600"
-            click={close}
+            onClick={close}
             className="mr-3"
           >
             <CloseRoundedIcon
@@ -70,17 +74,116 @@ const EPModalHeader: React.FC<EPModalHeaderProps> = ({
             Edit your Profile
           </h1>
         </div>
-        <div className="flex items-center mx-3">
-          <Button
-            size="big"
-            click={save}
-            className=""
-            disabled={disabled}
-            variant="primary"
-            text="Save"
-          />
+      </div>
+    </div>
+  );
+};
+
+interface EPModalBodyProps {
+  bannerUri: string | null;
+  displayedName: string;
+  bio: string | null;
+}
+
+const EPModalBody: React.FC<EPModalBodyProps> = ({
+  bannerUri,
+  displayedName,
+  bio,
+}) => {
+  const [session] = useSession();
+  const [bUri, setbUri] = useState<string>(bannerUri === null ? "" : bannerUri);
+  const [dName, setdName] = useState<string>(displayedName);
+  const [newBio, setNewBio] = useState<string>(bio);
+
+  const [updateProfile] = useMutation<string, OperationVariables>(
+    UPDATE_PROFILE
+  );
+
+  const handleError = () => {
+    return bUri.length > 255 || dName.length > 25 || newBio.length > 160;
+  };
+
+  const handleSubmit = async (event) => {
+    await updateProfile({
+      variables: {
+        userId: window.sessionStorage.getItem("UID"),
+        displayName: dName,
+        bio: newBio,
+        bannerUrl: bUri,
+      },
+    });
+    event.preventDefault();
+  };
+
+  return (
+    <div className="flex flex-col items-start justify-center w-full h-full py-4 px-3">
+      <div className="flex flex-row w-full items-center justify-start">
+        <SingleUserAvatar
+          src={session.user.image}
+          size="big"
+          alt="Current User Avatar"
+          className="mr-4"
+        />
+        <div className="flex flex-col w-full items-start justify-start">
+          <p className="text-xl font-semibold text-primary-200">
+            @{session.user.name}
+          </p>
+          <p className="text-base font-semibold text-primary-200">
+            {session.user.email}
+          </p>
         </div>
       </div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex  flex-col w-full h-full items-start justify-center bg-transparent relative"
+      >
+        <InputField
+          name="bannerUri-input"
+          counter={255}
+          textarea
+          label="Banner Uri"
+          placeholder="Add an image url to edit your Banner"
+          className="mt-5"
+          value={bUri}
+          onChange={(e) => {
+            setbUri(e.target.value);
+          }}
+          rows={4}
+        />
+        <InputField
+          name="displayName-input"
+          counter={25}
+          label="Username"
+          className="mt-5"
+          value={dName}
+          onChange={(e) => {
+            setdName(e.target.value);
+          }}
+        />
+        <InputField
+          name="status-input"
+          counter={160}
+          textarea
+          label="Status"
+          className="mt-5"
+          value={newBio}
+          onChange={(e) => {
+            setNewBio(e.target.value);
+          }}
+          rows={3}
+        />
+        <div className="flex w-full h-full justify-end flex-row mt-5">
+          <Button
+            size="big"
+            onClick={() => {}}
+            className=""
+            disabled={handleError()}
+            variant="primary"
+            text="Save"
+            type="submit"
+          />
+        </div>
+      </form>
     </div>
   );
 };
