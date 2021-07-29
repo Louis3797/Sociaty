580859d48a525cf65ab2eff329966ab736225c27
Louis <53270473+Louis3797@users.noreply.github.com>
2021-07-29 17:26:42 +0200

    fixed and updated prisma and db

--- bisasam/prisma/migrations/20210622163540_updated_schema_1_5/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateIndex
-CREATE INDEX `User_liked_comment.userId_comment_id_index` ON `User_liked_comment`(`userId`, `comment_id`);
-
--- CreateIndex
-CREATE INDEX `User_liked_content.userId_content_id_index` ON `User_liked_content`(`userId`, `content_id`);
