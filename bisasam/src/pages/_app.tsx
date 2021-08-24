0e38ab3b3629644cc63888a4ad5e8b97f4adfc39
Louis <53270473+Louis3797@users.noreply.github.com>
2021-08-24 16:42:20 +0200

    added favicon, head on _app and _document

--- bisasam/src/pages/_app.tsx
+++ bisasam/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import { AppProps } from "next/app";
 import { Provider } from "next-auth/client";
 import "../styles/globals.css";
@@ -11,34 +12,57 @@ import NavLayout from "../components/layouts/NavLayout";
 function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
   return (
-    <Provider
-      // Provider options are not required but can be useful in situations where
-      // you have a short session maxAge time. Shown here with default values.
-      options={{
-        // Client Max Age controls how often the useSession in the client should
-        // contact the server to sync the session state. Value in seconds.
-        // e.g.
-        // * 0  - Disabled (always use cache value)
-        // * 60 - Sync session state with server if it's older than 60 seconds
-        clientMaxAge: 60,
-        // Keep Alive tells windows / tabs that are signed in to keep sending
-        // a keep alive request (which extends the current session expiry) to
-        // prevent sessions in open windows from expiring. Value in seconds.
-        //
-        // Note: If a session has expired when keep alive is triggered, all open
-        // windows / tabs will be updated to reflect the user is signed out.
-        keepAlive: 0,
-      }}
-      session={pageProps.session}
-    >
-      <SessionLayout>
-        <ApolloProvider client={apolloClient}>
-          <NavLayout>
-            <Component {...pageProps} />
-          </NavLayout>
-        </ApolloProvider>
-      </SessionLayout>
-    </Provider>
+    <>
+      <Head>
+        <title>sociaty - the peoples network</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="assets/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="assets/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="assets/favicon-16x16.png"
+        />
+      </Head>
+      <Provider
+        // Provider options are not required but can be useful in situations where
+        // you have a short session maxAge time. Shown here with default values.
+        options={{
+          // Client Max Age controls how often the useSession in the client should
+          // contact the server to sync the session state. Value in seconds.
+          // e.g.
+          // * 0  - Disabled (always use cache value)
+          // * 60 - Sync session state with server if it's older than 60 seconds
+          clientMaxAge: 60,
+          // Keep Alive tells windows / tabs that are signed in to keep sending
+          // a keep alive request (which extends the current session expiry) to
+          // prevent sessions in open windows from expiring. Value in seconds.
+          //
+          // Note: If a session has expired when keep alive is triggered, all open
+          // windows / tabs will be updated to reflect the user is signed out.
+          keepAlive: 0,
+        }}
+        session={pageProps.session}
+      >
+        <SessionLayout>
+          <ApolloProvider client={apolloClient}>
+            <NavLayout>
+              <Component {...pageProps} />
+            </NavLayout>
+          </ApolloProvider>
+        </SessionLayout>
+      </Provider>
+    </>
   );
 }
 
