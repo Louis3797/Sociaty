a8ce320e680317862305f4906c7ef722aab04393
Louis <53270473+Louis3797@users.noreply.github.com>
2021-08-24 16:41:34 +0200

    updated input and inputfield

--- bisasam/src/components/elements/input/Input.tsx
+++ bisasam/src/components/elements/input/Input.tsx
@@ -2,11 +2,11 @@ import React, { forwardRef, useEffect, useState } from "react";
 
 export interface InputProps extends React.ComponentPropsWithoutRef<"input"> {
   textarea?: boolean;
-  rows: number;
+  rows?: number;
 }
 export const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ className, textarea, rows, ...props }, ref) => {
-    const style = `w-full mb-2 text-secondary placeholder-primary-300 focus:outline-none bg-transparent`;
+    const style = `w-full text-secondary placeholder-primary-200 focus:outline-none bg-transparent`;
 
     return textarea ? (
       <textarea
