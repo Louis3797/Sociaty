c4484acf66f1e3fc00fb376e4efb507547344421
Louis <53270473+Louis3797@users.noreply.github.com>
2021-08-30 16:55:56 +0200

    reworked prismaClient

--- bisasam/src/lib/prismaClient.ts
+++ bisasam/src/lib/prismaClient.ts
@@ -1,13 +1,36 @@
+// import { Prisma, PrismaClient } from "@prisma/client";
+
+// const prisma: PrismaClient<
+//   Prisma.PrismaClientOptions,
+//   never,
+//   Prisma.RejectOnNotFound | Prisma.RejectPerOperation
+// > = new PrismaClient<
+//   Prisma.PrismaClientOptions,
+//   never,
+//   Prisma.RejectOnNotFound | Prisma.RejectPerOperation
+// >();
+
+// export default prisma;
+
 import { Prisma, PrismaClient } from "@prisma/client";
 
-const prisma: PrismaClient<
+declare global {
+  var prismaa: PrismaClient | undefined;
+}
+
+let prisma: PrismaClient<
   Prisma.PrismaClientOptions,
   never,
   Prisma.RejectOnNotFound | Prisma.RejectPerOperation
-> = new PrismaClient<
-  Prisma.PrismaClientOptions,
-  never,
-  Prisma.RejectOnNotFound | Prisma.RejectPerOperation
->();
+>;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prismaa) {
+    global.prismaa = new PrismaClient();
+  }
+  prisma = global.prismaa;
+}
 
 export default prisma;
