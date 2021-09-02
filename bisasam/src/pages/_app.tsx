f67d416a609937b1306538c89e48b97f683f28a8
Louis <53270473+Louis3797@users.noreply.github.com>
2021-09-02 20:59:22 +0200

    changes on favicon

--- bisasam/src/pages/_app.tsx
+++ bisasam/src/pages/_app.tsx
@@ -17,23 +17,6 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>sociaty - the peoples network</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="assets/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="assets/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="assets/favicon-16x16.png"
-        />
       </Head>
       <Provider
         // Provider options are not required but can be useful in situations where
--- bisasam/src/pages/_document.tsx
+++ bisasam/src/pages/_document.tsx
@@ -18,6 +18,24 @@ class MyDocument extends Document {
         <Head>
           {/* <link rel="canonical" href="http://example.com/" /> */}
           <link rel="manifest" href="/site.webmanifest" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="assets/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="assets/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="assets/favicon-16x16.png"
+          />
+          <link rel="shortcut icon" href="/assets/favicon.ico" />
         </Head>
         <body>
           <Main />
