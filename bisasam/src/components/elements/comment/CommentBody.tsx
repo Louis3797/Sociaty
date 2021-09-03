5b8243296ee082df9d833c204d6b83fe2402004f
Louis <53270473+Louis3797@users.noreply.github.com>
2021-09-03 14:01:36 +0200

    added tokenized Text

--- bisasam/src/components/elements/comment/CommentBody.tsx
+++ bisasam/src/components/elements/comment/CommentBody.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import React from "react";
+import { useTokenizeText } from "../../../hooks/useTokenizeText";
+import { useTokenToText } from "../../../hooks/useTokenToText";
 
 export interface CommmentBodyProps {
   text: string;
@@ -7,11 +9,14 @@ export interface CommmentBodyProps {
 }
 
 const CommmentBody: React.FC<CommmentBodyProps> = ({ text, gifUrl }) => {
+  const [tokens] = useTokenizeText(text);
+
+  const [newText] = useTokenToText(tokens);
   return (
     <>
       <div className="w-full h-auto bg-primary-800 mt-1 rounded-8">
         <p className="break-words mr-5 ml-4 mb-1 text-secondary text-opacity-80 font-medium">
-          {text}
+          <div dangerouslySetInnerHTML={{ __html: newText }} />
         </p>
         {gifUrl?.length > 0 && (
           <div className="flex w-full h-auto justify-end px-4 mb-1 rounded-20">
