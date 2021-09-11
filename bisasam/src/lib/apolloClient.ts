6fa660bb3cc64748d1aa7c6b8503b53509303813
Louis <53270473+Louis3797@users.noreply.github.com>
2021-09-11 21:24:37 +0200

    updated types

--- bisasam/src/lib/apolloClient.ts
+++ bisasam/src/lib/apolloClient.ts
@@ -1,7 +1,12 @@
 import { useMemo } from "react";
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from "@apollo/client";
 
-let apolloClient;
+let apolloClient: ApolloClient<NormalizedCacheObject>;
 
 function createApolloClient() {
   return new ApolloClient({
@@ -14,7 +19,9 @@ function createApolloClient() {
   });
 }
 
-export function initializeApollo(initialState = null) {
+export function initializeApollo(
+  initialState: NormalizedCacheObject | null | undefined = null
+) {
   const _apolloClient = apolloClient ?? createApolloClient();
 
   // If your page has Next.js data fetching methods that use Apollo Client, the initial state
@@ -30,7 +37,9 @@ export function initializeApollo(initialState = null) {
   return _apolloClient;
 }
 
-export function useApollo(initialState) {
+export function useApollo(
+  initialState: NormalizedCacheObject | null | undefined
+) {
   const store = useMemo(() => initializeApollo(initialState), [initialState]);
   return store;
 }
