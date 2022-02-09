0b4471b88cb56608547d1a8c0302f2b16bac34ce
Louis <53270473+Louis3797@users.noreply.github.com>
2022-02-09 17:51:29 +0100

    added loading and route to user

--- bisasam/src/components/elements/searchbar/DataList.tsx
+++ bisasam/src/components/elements/searchbar/DataList.tsx
@@ -1,4 +1,5 @@
-import { User } from "@prisma/client";
+import { CircularProgress } from "@material-ui/core";
+import { useRouter } from "next/router";
 import React from "react";
 import NotFoundItem from "./NotFoundItem";
 import ResultItem from "./ResultItem";
@@ -11,18 +12,32 @@ interface DataListProps {
     image: string | null;
     online: boolean | null;
   }[];
+  loading: boolean;
 }
 
-const DataList: React.FC<DataListProps> = ({ user }) => {
+const DataList: React.FC<DataListProps> = ({ user, loading }) => {
+  const router = useRouter();
   return (
     <div className="flex flex-col h-auto bg-primary-600 w-full rounded-5 transition duration-1000 ease-in-out ring-1 ring-primary-300">
-      {!!user && user.length > 0 ? (
+      {loading == true ? (
+        <div className="flex w-full h-full items-center justify-center bg-transparent">
+          <CircularProgress />
+        </div>
+      ) : !!user && user.length > 0 ? (
         user.map((u) => (
           <ResultItem
             name={u.name}
             displayName={u.displayName}
             image={u.image}
             key={u.id}
+            onClick={() =>
+              (window.location.href = `/u/${encodeURIComponent(
+                decodeURIComponent(
+                  // @ts-ignore
+                  u.displayName
+                ).replace(/\s+/g, "")
+              )}`)
+            }
           />
         ))
       ) : (
