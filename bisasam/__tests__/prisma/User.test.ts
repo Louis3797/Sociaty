dc78c488ede16118f7ae994fb365339da8169e08
Louis <53270473+Louis3797@users.noreply.github.com>
2022-02-10 12:24:07 +0100

    tests

---
+++ bisasam/__tests__/prisma/User.test.ts
@@ -0,0 +1,41 @@
+import { prismaMock } from "../../src/lib/singleton";
+
+// test('should create new user ', async () => {
+//     const displayedName = "John"
+//     const user = {
+//         id: "25jvbfd747r3vjfhby6",
+//         name: "John1",
+//         displayName: "John",
+//         email: "john123@gmail.com",
+//         emailVerified: null,
+//         created_at: "2021-09-14T07:42:29.000Z",
+//         updatedAt: "2021-09-14T07:42:29.000Z",
+//         image: "https://test.test.com/wrtynbffre24f5t4df",
+//         bannerUrl: "",
+//         bio: "Hey im ne here !",
+//         numFollowings: 0,
+//         numFollowers: 0,
+//         online: false,
+//         numContributions: 0,
+//         content: [],
+//         comments: [],
+//         messages: [],
+//         liked_content: [],
+//         in_group: [],
+//         liked_comments: [],
+//         blocked_by: [],
+//         blocked: [],
+//         followedBy: [],
+//         following: [],
+//         accounts: [],
+//         sessions: [],
+//     }
+
+//     prismaMock.user.findFirst.mockResolvedValue(user)
+
+//     await expect(createUser(user)).resolves.toEqual({
+//       id: 1,
+//       name: 'Rich',
+//       email: 'hello@prisma.io',
+//     })
+//   })
